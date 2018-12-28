96eaf244428dc0c0f33a736ee80c9d26f2599e93
Suresh <suresh.seeni@arivistadigital.com>
2018-12-28 10:53:49 +0530

    Arivista Game Project with clone

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-MenkalaiBaloon
+# BalloonGame
+Google play link: https://play.google.com/store/apps/details?id=com.rohan.balloongame
+
+Please test and suggest improvements :)
+
+Simple balloon popping game for learning animations in Android
+
+<b>Screenshots</b>
+
+![Balloon Game](screenshots/Screenshot_20170522-035135.png)
+
+![Balloon Game](screenshots/Screenshot_20170522-035141.png)
+
+Tutorial: https://www.linkedin.com/learning/building-a-game-app-with-the-android-sdk
---
+++ app/src/main/java/menkalai/baloongame/utils/AppHelper.java
@@ -0,0 +1,47 @@
+package menkalai.baloongame.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Rohan on 22-May-17.
+ */
+
+public class AppHelper {
+
+    private static final String PREFS_GLOBAL = "prefs_global";
+    private static final String PREF_TOP_SCORE = "pref_top_score";
+    private static final String PREG_LEVEL="pref_level";
+
+    private static SharedPreferences getPreferences(Context context) {
+        return context.getSharedPreferences(
+                PREFS_GLOBAL, Context.MODE_PRIVATE);
+    }
+
+    //  Setters and getters for global preferences
+    public static boolean isTopScore(Context context, int newScore) {
+        int topScore = getPreferences(context).getInt(PREF_TOP_SCORE, 0);
+        return newScore > topScore;
+    }
+
+    public static int getTopScore(Context context) {
+        return getPreferences(context).getInt(PREF_TOP_SCORE, 0);
+    }
+
+    //  Setters and getters for global preferences
+    public static boolean isLevel(Context context, int newLevel) {
+        int topLevel = getPreferences(context).getInt(PREG_LEVEL, 0);
+        return newLevel > topLevel;
+    }
+
+    public static int getLevel(Context context) {
+        return getPreferences(context).getInt(PREG_LEVEL, 0);
+    }
+
+    public static void setTopScore(Context context, int score,int level) {
+        SharedPreferences.Editor editor = getPreferences(context).edit();
+        editor.putInt(PREF_TOP_SCORE, score);
+        editor.putInt(PREG_LEVEL, level);
+        editor.apply();
+    }
+}
